3c24aaaeb7d2675091ee1e52756bc9c364bd349e
JDomergue91 <domerguejonathan91@gmail.com>
2022-11-22 15:26:03 +0100

    euroMillions_refacto

--- euromillions.js
+++ euromillions.js
@@ -1,48 +1,31 @@
 let numerosRond = [];
 let numerosEtoile = [];
-
 const numerosTirage = document.getElementById('numerosTirage');
-const divNumeros = document.querySelectorAll('.numeros'); 
-
-
 window.addEventListener("load", (event) => {
-
-
 while (numerosRond.length < 5) {
     let newNumberRond = Math.ceil(Math.random() * 50);
     if (numerosRond.indexOf(newNumberRond) == -1) {
         numerosRond.push(newNumberRond);
     }
 }
-
 console.log(numerosRond);
-
 while (numerosEtoile.length < 2) {
     let newNumberEtoile = Math.ceil(Math.random() * 12);
     if (numerosEtoile.indexOf(newNumberEtoile) == -1) {
         numerosEtoile.push(newNumberEtoile);
     }
 }
-
 console.log(numerosEtoile)
-
-
 // creation dynamique des numeros du tirage
-
 numerosRond.forEach((rond, index) =>{
-
     const divRond = document.createElement('div');
     divRond.classList.add("ronds", "numeros");
     divRond.textContent = rond;
     setTimeout(() => {
         divRond.style.visibility = 'visible';
-    }, 500*index);
-   
+    }, 500*index); 
     numerosTirage.appendChild(divRond);
-
 })
-
-
 // creation dynamique des numeros du tirage
 numerosEtoile.forEach((etoile, index )=>{
 
@@ -52,13 +35,6 @@ numerosEtoile.forEach((etoile, index )=>{
     setTimeout(() => {
         divEtoile.style.visibility = 'visible';
     }, 2500 + 500* index);
-
     numerosTirage.appendChild(divEtoile);
 })
-
-
-
-
-
-
 });
